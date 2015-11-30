51935676708f7b40a7f9d18fc901447074bbd209
Matt Anderson <matthewia94@gmail.com>
2015-11-30 00:33:31 -0600

    Add navigation node
    The python script currently just spins the robot in circles but
    is setup to operate like a real ros node.  It will need to run the
    nueral net still.

---
+++ ros_ws/src/navigation/scripts/navigation_node.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+
+class Navigation:
+    def __init__(self):
+        self.dists = list()
+
+        # ROS initialization
+        rospy.init_node('navigation', anonymous=True)
+        rospy.Subscriber("/scan", LaserScan, self.laserCallback)
+        self.pub = rospy.Publisher('/auto_twist', Twist, queue_size=1)
+
+        self.twist_msg = Twist()
+
+    def laserCallback(self, data):
+        self.dists = data.ranges
+
+    def update(self):
+        self.twist_msg.angular.z = 1
+        self.pub.publish(self.twist_msg)
+
+if __name__ == '__main__':
+    n = Navigation()
+
+    rate = rospy.Rate(30)
+    while not rospy.is_shutdown():
+        n.update()
+        rate.sleep()

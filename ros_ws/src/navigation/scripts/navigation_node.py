bf4cac97e7c1a241a2f347ca4f11aecf6b09f501
Matt Anderson <matthewia94@gmail.com>
2015-11-30 22:54:23 -0600

    Add function to reset robot position
    The robot position is reset when using the resetRobot function.

--- ros_ws/src/navigation/scripts/navigation_node.py
+++ ros_ws/src/navigation/scripts/navigation_node.py
@@ -3,32 +3,47 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 from NeuralNetwork import NeuralNetwork
+from gazebo_msgs.msg import ModelState
+from geometry_msgs.msg import Pose
 
 
 class Navigation:
     def __init__(self):
-        self.dists = list()
+        self.network = NeuralNetwork(540, 550, 2)
 
         # ROS initialization
         rospy.init_node('navigation', anonymous=True)
         rospy.Subscriber("/scan", LaserScan, self.laserCallback)
         self.pub = rospy.Publisher('/auto_twist', Twist, queue_size=1)
 
+        # Pose publisher for setting the pose of the robot
+        self.pose_pub = rospy.Publisher('gazebo/set_model_state', ModelState, queue_size=1)
+
         self.twist_msg = Twist()
 
-        self.net = NeuralNetwork(1080, 1100, 2)
-
     def laserCallback(self, data):
-        self.dists = data.ranges
+        # Set the input layer equal to the distances of the lidar
+        self.network.input_layer = data.ranges[0::2]
 
     def update(self):
         self.twist_msg.angular.z = 1
         self.pub.publish(self.twist_msg)
+
+    def resetRobot(self):
+        s = ModelState()
+        s.model_name = 'igvc'
+        p = Pose()
+        p.position.x = -2
+        p.position.y = -21.5
+        p.orientation.w = -1
+        s.pose = p
+        self.pose_pub.publish(s)
 
 if __name__ == '__main__':
     n = Navigation()
 
     rate = rospy.Rate(30)
     while not rospy.is_shutdown():
+        n.resetRobot()
         n.update()
         rate.sleep()

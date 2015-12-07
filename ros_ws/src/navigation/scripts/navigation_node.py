d12eaaf9459c0e820994d3a711d052026aa11976
Matt Anderson <matthewia94@gmail.com>
2015-12-07 14:23:54 -0600

    Create and display membership functions
    The membership functions for the robot are created and
    displayed.

--- ros_ws/src/navigation/scripts/navigation_node.py
+++ ros_ws/src/navigation/scripts/navigation_node.py
@@ -2,17 +2,32 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
-from NeuralNetwork import NeuralNetwork
 from gazebo_msgs.msg import ModelState
 from gazebo_msgs.msg import ContactsState
 from geometry_msgs.msg import Pose
 import numpy as np
-import time
-
+import skfuzzy as fuzz
+import matplotlib.pyplot as plt
 
 class Navigation:
     def __init__(self):
-        self.network = NeuralNetwork(540, 541, 2)
+        self.dists = list()
+
+        # The number of distance fields
+        self.num_fields = 5
+        self.dists_obstacles = np.arange(0, 30, 1)
+        self.goal_dist = np.arange(0, 100, 1)
+
+        # Membership functions
+        self.goal_dist_far = fuzz.trapmf(self.goal_dist, [7, 10, 100, 100])
+        self.goal_dist_close = fuzz.trapmf(self.goal_dist, [0, 0, 10, 12])
+        self.obstacle_dist_very_close = fuzz.trapmf(self.dists_obstacles, [0, 0, 6, 7])
+        self.obstacle_dist_close = fuzz.trapmf(self.dists_obstacles, [5, 6, 12, 13])
+        self.obstacle_dist_moderate = fuzz.trapmf(self.dists_obstacles, [11, 12, 18, 19])
+        self.obstacle_dist_far = fuzz.trapmf(self.dists_obstacles, [17, 18, 24, 25])
+        self.obstacle_dist_very_far = fuzz.trapmf(self.dists_obstacles, [23, 24, 30, 30])
+
+        self.display_members()
 
         # ROS initialization
         rospy.init_node('navigation', anonymous=True)
@@ -25,23 +40,44 @@ class Navigation:
 
         self.twist_msg = Twist()
 
+    def display_members(self):
+        fig, (mem1, mem2) = plt.subplots(nrows=2)
+        mem1.plot(self.goal_dist, self.goal_dist_far, 'b', linewidth=2, label='Far')
+        mem1.plot(self.goal_dist, self.goal_dist_close, 'g', linewidth=2, label='Close')
+        mem1.legend()
+        mem1.spines['top'].set_visible(False)
+        mem1.spines['right'].set_visible(False)
+        mem1.get_xaxis().tick_bottom()
+        mem1.get_yaxis().tick_left()
+
+        mem2.plot(self.dists_obstacles, self.obstacle_dist_very_close, 'b', linewidth=2, label='Very Close')
+        mem2.plot(self.dists_obstacles, self.obstacle_dist_close, 'g', linewidth=2, label='Close')
+        mem2.plot(self.dists_obstacles, self.obstacle_dist_moderate, 'r', linewidth=2, label='Moderate')
+        mem2.plot(self.dists_obstacles, self.obstacle_dist_far, 'm', linewidth=2, label='Far')
+        mem2.plot(self.dists_obstacles, self.obstacle_dist_very_far, 'y', linewidth=2, label='Very Far')
+        mem2.legend()
+        mem2.spines['top'].set_visible(False)
+        mem2.spines['right'].set_visible(False)
+        mem2.get_xaxis().tick_bottom()
+        mem2.get_yaxis().tick_left()
+        plt.tight_layout()
+        plt.show()
+
     def laserCallback(self, data):
         # Set the input layer equal to the distances of the lidar normalized
-        self.network.input_layer = np.array(data.ranges[0::2]) / data.range_max
-        self.network.feedforward()
-        self.network.output_layer[0] = self.network.output_layer[0]/max(self.network.output_layer)
-        self.network.output_layer[1] = self.network.output_layer[1]/max(self.network.output_layer)
-        self.network.backpropagation(self.error())
+        self.dists = []
+        scan = list(data.ranges[270:810])
+        for i in range(self.num_fields):
+            self.dists.append(min(scan[i*(len(scan)/self.num_fields):(i+1)*(len(scan)/self.num_fields)]))
 
     def update(self):
-        self.twist_msg.linear.x = self.network.output_layer[0]/max(self.network.output_layer)
-        self.twist_msg.angular.z = self.network.output_layer[1]/max(self.network.output_layer)
+        self.twist_msg.linear.x = 0
+        self.twist_msg.angular.z = 0
         self.pub.publish(self.twist_msg)
-        print self.network.output_layer
 
     def bumpCallback(self, data):
         if len(data.states) > 0:
-            self.resetRobot()rest
+            self.resetRobot()
 
     def resetRobot(self):
         s = ModelState()
@@ -51,9 +87,6 @@ class Navigation:
         p.position.y = -21.5
         s.pose = p
         self.pose_pub.publish(s)
-
-    def error(self):
-        return [min(1/self.network.input_layer)] * self.network.num_outputs
 
 if __name__ == '__main__':
     n = Navigation()
